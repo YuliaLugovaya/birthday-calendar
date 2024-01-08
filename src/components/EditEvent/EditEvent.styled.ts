375d0127d3c9ff9f95f8ca03e9b671e6461b538e
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2024-01-08 18:46:48 +0300

    feat: add select events

--- src/components/EditEvent/EditEvent.styled.ts
+++ src/components/EditEvent/EditEvent.styled.ts
@@ -4,6 +4,7 @@ export const styles = {
       p: "20px",
       width: "100vw",
       height: "100vh",
+      alignItems: "start",
     },
   },
   editEventIconClose: {
@@ -15,5 +16,31 @@ export const styles = {
   },
   editEventTitle: {
     fontSize: "2rem",
+  },
+  editEventAdd: {
+    width: "300px",
+    p: "20px",
+    border: "1px solid",
+    borderColor: "color.other",
+    bgcolor: "color.greenLight",
+    color: "text.primary",
+    "&:hover": {
+      bgcolor: "color.other",
+      color: "text.secondary",
+    },
+  },
+  editEventChange: {
+    width: "300px",
+    border: "1px solid",
+    borderColor: "color.other",
+    borderRadius: "4px",
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      border: "1px solid",
+      borderColor: "color.other",
+    },
+    "&:hover": {
+      border: "1px solid",
+      borderColor: "color.other",
+    },
   },
 };
--- src/components/EditEvent/EditEvent.tsx
+++ src/components/EditEvent/EditEvent.tsx
@@ -1,8 +1,17 @@
-import React, { FC } from "react";
-import { Box, CardMedia, Drawer, Typography } from "@mui/material";
+import React, { FC, useState } from "react";
+import {
+  Box,
+  Button,
+  CardMedia,
+  Drawer,
+  MenuItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { styles } from "./EditEvent.styled";
 import { IEditEventProps } from "./EditEventTypes";
 import close from "assets/images/png/close.png";
+import events from "./events";
 
 export const EditEvent: FC<IEditEventProps> = ({
   isDrawerOpen,
@@ -21,6 +30,12 @@ export const EditEvent: FC<IEditEventProps> = ({
   }
   modifiedMonth =
     modifiedMonth.charAt(0).toLowerCase() + modifiedMonth.slice(1);
+
+  const [isEventAdded, setIsEventAdded] = useState(false);
+
+  const handleAddEvent = () => {
+    setIsEventAdded(true);
+  };
 
   return (
     <Drawer
@@ -41,6 +56,29 @@ export const EditEvent: FC<IEditEventProps> = ({
           {day} {modifiedMonth} {year}
         </Typography>
       </Box>
+      {isEventAdded ? (
+        <TextField
+          select
+          sx={styles.editEventChange}
+          defaultValue="Выберите событие"
+        >
+          {events.map((option) => (
+            <MenuItem
+              key={option.value}
+              value={option.value}
+              disabled={option.disabled}
+            >
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      ) : (
+        <>
+          <Button onClick={handleAddEvent} sx={styles.editEventAdd}>
+            Добавить событие
+          </Button>
+        </>
+      )}
     </Drawer>
   );
 };
